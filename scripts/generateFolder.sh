db9d64e97b3b642fc28dc9b81650b06707e40326
Breena Lockser <pi17yjo@eduvaud.ch>
2025-10-06 08:23:14 +0200

    Initial commit

---
+++ scripts/generateFolder.sh
@@ -0,0 +1,2 @@
+#!bash
+mkdir -p app/{wordpress,nginx} app/wordpress/themes/plugins config/{mysql,wordpress} docker/{wordpress,mysql,nginx} prometheus grafana/{datasources,dashboards} docs/ scripts/
